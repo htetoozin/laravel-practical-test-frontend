d3598dd23f7deb9a285bb848db863b9a4d3bd1f0
htetoozin <htetoo.zin09@gmail.com>
2023-05-09 00:41:18 +0630

    [create] login page

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import {
   faPaperPlane,
 } from "@fortawesome/free-solid-svg-icons";
 import "./App.css";
+import Login from "./pages/Login";
 
 function App() {
   const [formTitle, setFormTitle] = useState("Untitled Form");
@@ -47,96 +48,97 @@ function App() {
   };
 
   return (
-    <Container>
-      <div className="mt-10 col-sm-12 col-md-12">
-        <Card>
-          <Card.Header className="card_header">
-            <Col md="3" lg="3">
-              <InputGroup>
-                <Form.Control
-                  placeholder="Untitled form"
-                  aria-describedby="basic-addon1"
-                  value={formTitle}
-                  onChange={(e) => setFormTitle(e.target.value)}
-                />
-              </InputGroup>
-            </Col>
-          </Card.Header>
-          <Card.Body>
-            <div className="row mb-3 p-b-10 border-bottom border-color border-info bp-9">
-              <Col md="3" lg="3">
-                <Form.Select
-                  aria-label="Default select box"
-                  onChange={(e) => setSelectType(e.target.value)}
-                >
-                  <option value="text">Text</option>
-                  <option value="number">Number</option>
-                  <option value="date">Date Picker</option>
-                </Form.Select>
-              </Col>
-              <Col md="2" lg="2">
-                <Button variant="primary" onClick={addRowHandler}>
-                  <FontAwesomeIcon icon={faPlusCircle} />
-                  <span className="ml-9">Add</span>
-                </Button>
-              </Col>
-            </div>
-            <form onSubmit={submitHandler}>
-              {formFields.map((form, index) => (
-                <div className="row" key={index}>
-                  <Col md="1" lg="1">
-                    <Button variant="secondary">
-                      <span className="form_type">{form.type}</span>
-                    </Button>
-                  </Col>
+    <Login />
+    // <Container>
+    //   <div className="mt-10 col-sm-12 col-md-12">
+    //     <Card>
+    //       <Card.Header className="card_header">
+    //         <Col md="3" lg="3">
+    //           <InputGroup>
+    //             <Form.Control
+    //               placeholder="Untitled form"
+    //               aria-describedby="basic-addon1"
+    //               value={formTitle}
+    //               onChange={(e) => setFormTitle(e.target.value)}
+    //             />
+    //           </InputGroup>
+    //         </Col>
+    //       </Card.Header>
+    //       <Card.Body>
+    //         <div className="row mb-3 p-b-10 border-bottom border-color border-info bp-9">
+    //           <Col md="3" lg="3">
+    //             <Form.Select
+    //               aria-label="Default select box"
+    //               onChange={(e) => setSelectType(e.target.value)}
+    //             >
+    //               <option value="text">Text</option>
+    //               <option value="number">Number</option>
+    //               <option value="date">Date Picker</option>
+    //             </Form.Select>
+    //           </Col>
+    //           <Col md="2" lg="2">
+    //             <Button variant="primary" onClick={addRowHandler}>
+    //               <FontAwesomeIcon icon={faPlusCircle} />
+    //               <span className="ml-9">Add</span>
+    //             </Button>
+    //           </Col>
+    //         </div>
+    //         <form onSubmit={submitHandler}>
+    //           {formFields.map((form, index) => (
+    //             <div className="row" key={index}>
+    //               <Col md="1" lg="1">
+    //                 <Button variant="secondary">
+    //                   <span className="form_type">{form.type}</span>
+    //                 </Button>
+    //               </Col>
 
-                  <Col md="5" lg="5" className="mb-3">
-                    <InputGroup>
-                      <Form.Control
-                        placeholder="Title"
-                        aria-describedby="basic-addon1"
-                        name="title"
-                        value={form.title}
-                        onChange={(e) => handleFormChange(e, index)}
-                      />
-                    </InputGroup>
-                  </Col>
-                  <Col md="3" lg="3" className="mb-3">
-                    <Form.Select
-                      aria-label="Default select box"
-                      name="required"
-                      value={form.required}
-                      onChange={(e) => handleFormChange(e, index)}
-                    >
-                      <option value={false}>Optional</option>
-                      <option value={true}>Required</option>
-                    </Form.Select>
-                  </Col>
+    //               <Col md="5" lg="5" className="mb-3">
+    //                 <InputGroup>
+    //                   <Form.Control
+    //                     placeholder="Title"
+    //                     aria-describedby="basic-addon1"
+    //                     name="title"
+    //                     value={form.title}
+    //                     onChange={(e) => handleFormChange(e, index)}
+    //                   />
+    //                 </InputGroup>
+    //               </Col>
+    //               <Col md="3" lg="3" className="mb-3">
+    //                 <Form.Select
+    //                   aria-label="Default select box"
+    //                   name="required"
+    //                   value={form.required}
+    //                   onChange={(e) => handleFormChange(e, index)}
+    //                 >
+    //                   <option value={false}>Optional</option>
+    //                   <option value={true}>Required</option>
+    //                 </Form.Select>
+    //               </Col>
 
-                  <Col md="2" lg="2">
-                    <Button
-                      variant="danger"
-                      onClick={() => deleteRowHandler(index)}
-                    >
-                      <FontAwesomeIcon icon={faTrashAlt} />
-                    </Button>
-                  </Col>
-                </div>
-              ))}
-              <div className="border-top border-color border-info bt-9">
-                <Button variant="primary" type="submit">
-                  <FontAwesomeIcon icon={faPaperPlane} />
-                  <span className="ml-10">Submit</span>
-                </Button>
-              </div>
-            </form>
-          </Card.Body>
-          <Card.Footer className="text-muted text-center">
-            Developed by HOZ
-          </Card.Footer>
-        </Card>
-      </div>
-    </Container>
+    //               <Col md="2" lg="2">
+    //                 <Button
+    //                   variant="danger"
+    //                   onClick={() => deleteRowHandler(index)}
+    //                 >
+    //                   <FontAwesomeIcon icon={faTrashAlt} />
+    //                 </Button>
+    //               </Col>
+    //             </div>
+    //           ))}
+    //           <div className="border-top border-color border-info bt-9">
+    //             <Button variant="primary" type="submit">
+    //               <FontAwesomeIcon icon={faPaperPlane} />
+    //               <span className="ml-10">Submit</span>
+    //             </Button>
+    //           </div>
+    //         </form>
+    //       </Card.Body>
+    //       <Card.Footer className="text-muted text-center">
+    //         Developed by HOZ
+    //       </Card.Footer>
+    //     </Card>
+    //   </div>
+    // </Container>
   );
 }
 
---
+++ src/pages/Login.js
@@ -0,0 +1,83 @@
+import { useState } from "react";
+
+import { Button } from "react-bootstrap";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import Card from "react-bootstrap/Card";
+
+const Login = () => {
+  const [payload, setPayload] = useState({
+    email: "",
+    password: "",
+  });
+  const [errors, setErrors] = useState("");
+
+  const handleChange = (event) => {
+    setPayload({ ...payload, [event.target.id]: event.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("payload ", payload);
+  };
+  return (
+    <div className="container">
+      <div className="row align-items-center vh-100">
+        <div className="col-6 mx-auto">
+          <div className="card shadow border">
+            <div className="card-body d-flex flex-column align-items-center">
+              <Card className="col-md-12 row justify-content-center">
+                <Card.Body>
+                  <Card.Title className="login_title">Login</Card.Title>
+                  <Form onSubmit={handleSubmit}>
+                    <InputGroup className="mb-4 mt-4">
+                      <InputGroup.Text id="inputGroup-sizing-default">
+                        Email
+                      </InputGroup.Text>
+                      <Form.Control
+                        type="email"
+                        aria-label="Default"
+                        aria-describedby="inputGroup-sizing-default"
+                        id="email"
+                        onChange={handleChange}
+                        required
+                      />
+                      {errors.email && (
+                        <span id="email" className="text-danger">
+                          {errors.email}
+                        </span>
+                      )}
+                    </InputGroup>
+                    <InputGroup className="mb-5">
+                      <InputGroup.Text id="inputGroup-sizing-default">
+                        Password
+                      </InputGroup.Text>
+                      <Form.Control
+                        type="password"
+                        aria-label="Default"
+                        aria-describedby="inputGroup-sizing-default"
+                        id="password"
+                        onChange={handleChange}
+                        required
+                      />
+                      {errors.password && (
+                        <span id="password" className="text-danger">
+                          {errors.password}
+                        </span>
+                      )}
+                    </InputGroup>
+                    <Button type="submit" variant="primary">
+                      Login
+                    </Button>
+                  </Form>
+                </Card.Body>
+              </Card>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
